152be3fa2163db70042eda67e3e3639b4e56b844
HuiLs <dhqks507@gmail.com>
2024-05-17 17:21:59 +0900

    modify the code

--- Build_KG.py
+++ Build_KG.py
@@ -24,18 +24,21 @@ def parse_ext_tuple(rdf_string):
                 extracted.append(rdf_string[start:i+1])
     return extracted
 
-def add_incident(tx, Incident, pubPrivFlag, dateOccur,   designSafetyRev, investMethod, futureActionPlan, safetyMgmtPlans, workerNum, processRates, constrDuration, bidRate, constrCost, recurPrevMeasures, postAccMeasures, reportReasons, accidentCause, accidentHistory, specificCause, date, incidentTime):
+
+def add_incident(tx, Incident, pubPrivFlag, dateOccur, designSafetyRev, investMethod, futureActionPlan, safetyMgmtPlans,
+                 workerNum, processRates, constrDuration, bidRate, constrCost, recurPrevMeasures, postAccMeasures,
+                 reportReasons, accidentCause, accidentHistory, specificCause, incidentTime, state, district, site,
+                 places, humidity, temperature, weather):
     print(f"{Incident} added")
     query = f"""
-   CREATE (a:Incident {{name: '{Incident}', bidRate : '{bidRate}', constructionCost : '{constrCost}',start : '{constrDuration[0]}', end : '{constrDuration[1]}', processRates : '{processRates}', workerNum : '{workerNum}', pubprivFlag : '{pubPrivFlag}', safetyMgmtPlans : '{safetyMgmtPlans}', investMethod : '{investMethod}', designSafetyReview : '{designSafetyRev}', date : '{date}', incidentTime : '{incidentTime}'}})
-    MERGE (e:Date {{name: '{dateOccur}'}})
+    CREATE (a:Incident {{name: '{Incident}', bidRate : '{bidRate}', constructionCost : '{constrCost}',start : '{constrDuration[0]}', end : '{constrDuration[1]}', processRates : '{processRates}', workerNum : '{workerNum}', pubprivFlag : '{pubPrivFlag}', safetyMgmtPlans : '{safetyMgmtPlans}', investMethod : '{investMethod}', designSafetyReview : '{designSafetyRev}', date : '{dateOccur}', incidentTime : '{incidentTime}', state : '{state}', district : '{district}', site : '{site}', places : '{places}', humidity : '{humidity}', temperature : '{temperature}', weather : '{weather}'}})
     MERGE (j:FutureActionPlan {{name: '{futureActionPlan}'}})
     MERGE (s:RecurrencePrevention {{name: '{recurPrevMeasures}'}})
     MERGE (t:PostAccidentMeasures {{name: '{postAccMeasures}'}})
     MERGE (w:Cause {{name: '{accidentCause}'}})
     MERGE (x:AccidentHistory {{name: '{accidentHistory}'}})
+    MERGE (v:ReportReasons {{name: '{reportReasons}'}})
     MERGE (z:SpecificCause {{name: '{specificCause}'}})
-    MERGE (a)-[:OCCURRED_ON]->(e)
     MERGE (a)-[:HAS_FUTURE_ACTION_PLAN]->(j)
     MERGE (a)-[:IMPLEMENTED_RECURRENCE_PREVENTION]->(s)
     MERGE (a)-[:TOOK_POST_ACCIDENT_MEASURES]->(t)
@@ -44,24 +47,27 @@ def add_incident(tx, Incident, pubPrivFlag, dateOccur,   designSafetyRev, invest
     MERGE (a)-[:HAS_ACCIDENT_HISTORY]->(x)
     MERGE (a)-[:HAS_SPECIFIC_CAUSE]->(z)
     """
+
     tx.run(query)
 
-def add_ConstrCat(tx, incident, constrCat_Major, constrCat_Medium):
+
+def add_ConstrCat(tx, incident, dateOccur, constrCat_Major, constrCat_Medium):
     print(f"ConstrCat added")
     query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
-    MERGE(b:ConstrCat_Major {{name: '{constrCat_Major}'}})
-    MERGE(c:ConstrCat_Medium {{name: '{constrCat_Medium}'}})
-    MERGE (a)-[:IS_MAJOR_CAT]->(b)
-    MERGE (a)-[:IS_MEDIUM_CAT]->(c)
+    MATCH (a:Incident {{name: '{incident}', date : '{dateOccur}'}})
+    MERGE(ccmajor:ConstrCat_Major {{name: '{constrCat_Major}'}})
+    MERGE(ccmedium:ConstrCat_Medium {{name: '{constrCat_Medium}'}})
+    MERGE (a)-[:IS_MAJOR_CAT]->(ccmajor)
+    MERGE (a)-[:IS_MEDIUM_CAT]->(ccmedium)
     MERGE (b)-[:INCLUDES]->(c)
     """
     tx.run(query)
 
-def add_IncidentObj(tx, incident, incidentObj_Major, incidentObj_Medium):
+
+def add_IncidentObj(tx, incident, dateOccur, incidentObj_Major, incidentObj_Medium):
     print(f"IncidentObj added")
     query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
+    MATCH (a:Incident {{name: '{incident}', date : '{dateOccur}'}})
     MERGE(b:IncidentObj_Major {{name: '{incidentObj_Major}'}})
     MERGE(c:IncidentObj_Medium {{name: '{incidentObj_Medium}'}})
     MERGE (a)-[:IS_MAJOR_CAT]->(b)
@@ -70,33 +76,11 @@ def add_IncidentObj(tx, incident, incidentObj_Major, incidentObj_Medium):
     """
     tx.run(query)
 
-def add_WeatherCond(tx, incident,humidity, temperature, weather):
-    print(f"WeatherCond added")
-    query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
-    MERGE(b:Weather {{name: '{weather}'}})
-    MERGE(c:Humidity {{name: '{humidity}'}})
-    MERGE(d:Temperature {{name: '{temperature}'}})
-    MERGE (a)-[:HAS_WEATHER]->(b)
-    MERGE (a)-[:HAS_HUMIDITY]->(c)
-    MERGE (a)-[:HAS_TEMPERATURE]->(d)
-    MERGE (b)-[:same_incident]->(c)
-    MERGE (c)-[:same_incident]->(d)
-    """
-    tx.run(query)
 
-def add_Location(tx, incident ,site, places):
-    print(f"Location added")
-    query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
-    SET a.site = '{site}', a.places = '{places}'
-    """
-    tx.run(query)
-
-def add_FacilityType(tx, incident, facilityMajorCat, facilityMediumCat, facilitySmallCat):
+def add_FacilityType(tx, incident, dateOccur, facilityMajorCat, facilityMediumCat, facilitySmallCat):
     print(f"FacilityType added")
     query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
+    MATCH (a:Incident {{name: '{incident}', date : '{dateOccur}'}})
     MERGE(b:FacilityMajorCat {{name: '{facilityMajorCat}'}})
     MERGE(c:FacilityMediumCat {{name: '{facilityMediumCat}'}})
     MERGE(d:FacilitySmallCat {{name: '{facilitySmallCat}'}})
@@ -108,25 +92,20 @@ def add_FacilityType(tx, incident, facilityMajorCat, facilityMediumCat, facility
     """
     tx.run(query)
 
-def add_IncidentType(tx, incident, humanIncident, materialAccident):
+
+def add_IncidentType(tx, incident, dateOccur, humanIncident, materialAccident):
     print(f"IncidentType added")
     query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
-    MERGE(b:HumanIncident {{name: '{humanIncident}'}})
-    MERGE(c:MaterialAccident {{name: '{materialAccident}'}})
+    MATCH (a:Incident {{name: '{incident}', date : '{dateOccur}'}})
+    MERGE (b:HumanIncident {{name: '{humanIncident}'}})
+    MERGE (c:MaterialAccident {{name: '{materialAccident}'}})
+    WITH a,b,c
+    WHERE NOT (a)-[:IS_HUMAN_INCIDENT]->(b) AND NOT (a)-[:IS_MATERIAL_ACCIDENT]->(c) 
     MERGE (a)-[:IS_HUMAN_INCIDENT]->(b)
     MERGE (a)-[:IS_MATERIAL_ACCIDENT]->(c)
-    MERGE (b)-[:same_incident]->(c)
     """
     tx.run(query)
 
-def add_cityDistrict(tx, incident, state, district):
-    print(f"CityDistrict added")
-    query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
-    SET a.state = '{state}', a.district = '{district}'
-    """
-    tx.run(query)
 
 def add_accidentHistory(tx, accidentHistory, acccidentEnv, accidentTask, accidentType):
     print(f"AccidentHistory added")
@@ -143,10 +122,11 @@ def add_accidentHistory(tx, accidentHistory, acccidentEnv, accidentTask, acciden
     """
     tx.run(query)
 
-def add_damage(tx, incident, fatalities, injured, damageAmount, damageDesc):
+
+def add_damage(tx, incident, dateOccur, fatalities, injured, damageAmount, damageDesc):
     print(f"Damage added")
     query = f"""
-    MATCH (a:Incident {{name: '{incident}'}})
+    MATCH (a:Incident {{name: '{incident}', date : '{dateOccur}'}})
     MERGE(b:Fatalities {{name: 'fatalities', local: '{fatalities[0]}', foreign: '{fatalities[1]}'}})
     MERGE(c:Injured {{name: 'injured', local: '{injured[0]}', foreign: '{injured[1]}'}})
     MERGE(d:DamageAmount {{name: '{damageAmount}'}})
@@ -155,37 +135,39 @@ def add_damage(tx, incident, fatalities, injured, damageAmount, damageDesc):
     MERGE (a)-[:HAS_INJURED]->(c)
     MERGE (a)-[:HAS_DAMAGE_AMOUNT]->(d)
     MERGE (a)-[:HAS_DAMAGE_DESC]->(e)
-    MERGE (b)-[:same_incident]->(c)
-    MERGE (c)-[:same_incident]->(d)
-    MERGE (d)-[:same_incident]->(e)
     """
     tx.run(query)
 
 #Incident, pubPrivFlag, dateOccur,   designSafetyRev, investMethod, futureActionPlan, safetyMgmtPlans, workerNum, processRates, constrDuration, bidRate, constrCost, recurPrevMeasures, postAccMeasures, reportReasons, accidentCause, accidentHistory
+# Incident, pubPrivFlag, dateOccur,   designSafetyRev, investMethod, futureActionPlan, safetyMgmtPlans, workerNum, processRates, constrDuration, bidRate, constrCost, recurPrevMeasures, postAccMeasures, reportReasons, accidentCause, accidentHistory
 with greeter.session() as session:
     for i, row in df.iterrows():
-        durate = (row.constrDuration_s,row.constrDuration_e)
-        session.execute_write(add_incident, row.Incident, row.pubPrivFlag, row.dateOccur,   row.designSafetyRev, row.investMethod, row.futureActionPlan, row.safetyMgmtPlans, row.workerNum, row.processRates, durate, row.bidRate, row.constrCost, row.recurPrevMeasures, row.postAccMeasures, row.reportReasons, row.accidentCause, row.accidentHistory, row.specificCause, row.dateOccur, row.incidentTime)
+        durate = (row.constrDuration_s, row.constrDuration_e)
+        session.execute_write(add_incident, row.Incident, row.pubPrivFlag, row.dateOccur, row.designSafetyRev,
+                              row.investMethod, row.futureActionPlan, row.safetyMgmtPlans, row.workerNum,
+                              row.processRates, durate, row.bidRate, row.constrCost, row.recurPrevMeasures,
+                              row.postAccMeasures, row.reportReasons, row.accidentCause, row.accidentHistory,
+                              row.specificCause, row.incidentTime, row.cityDistrict_state, row.cityDistrict_district,
+                              row.Location_site, row.Location_places, row.weatherCond_humidity,
+                              row.weatherCond_temperature, row.weatherCond_weather)
 
-        session.execute_write(add_ConstrCat, row.Incident, row.constrCat_Major, row.constrCat_Medium)
+        session.execute_write(add_ConstrCat, row.Incident, row.dateOccur, row.constrCat_Major, row.constrCat_Medium)
 
-        session.execute_write(add_IncidentObj, row.Incident, row.incidentObj_Major, row.incidentObj_Medium)
+        session.execute_write(add_IncidentObj, row.Incident, row.dateOccur, row.incidentObj_Major,
+                              row.incidentObj_Medium)
 
-        session.execute_write(add_WeatherCond, row.Incident, row.weatherCond_humidity, row.weatherCond_temperature, row.weatherCond_weather)
+        session.execute_write(add_FacilityType, row.Incident, row.dateOccur, row.FacilityType_Major,
+                              row.FacilityType_Medium, row.FacilityType_Small)
 
-        session.execute_write(add_Location, row.Incident, row.Location_site, row.Location_places)
+        session.execute_write(add_IncidentType, row.Incident, row.dateOccur, row.humanIncident, row.materialAccident)
 
-        session.execute_write(add_FacilityType, row.Incident, row.FacilityType_Major, row.FacilityType_Medium, row.FacilityType_Small)
-
-        session.execute_write(add_IncidentType, row.Incident, row.humanIncident, row.materialAccident)
-
-        session.execute_write(add_cityDistrict, row.Incident, row.cityDistrict_state, row.cityDistrict_district)
-
-        accidentType, accidentTask ,acccidentEnv = parse_ext_tuple(row.accidentHistoryCls)
+        accidentType, accidentTask, acccidentEnv = parse_ext_tuple(row.accidentHistoryCls)
 
         session.execute_write(add_accidentHistory, row.accidentHistory, acccidentEnv, accidentTask, accidentType)
 
         fatalities = (row.fatalities_local, row.fatalities_foreigner)
         injured = (row.injured_local, row.injured_foreigner)
 
-        session.execute_write(add_damage, row.Incident, fatalities, injured, row.damageAmount, row.damageDesc)
+        session.execute_write(add_damage, row.Incident, row.dateOccur, fatalities, injured, row.damageAmount,
+                              row.damageDesc)
+
